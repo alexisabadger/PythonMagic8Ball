ba446b8a2d9697133f7afbd02b9cd0e034851a02
alex (is a badger, an IRL badger) <73854333+alexisabadger@users.noreply.github.com>
2020-11-08 09:00:33 -0500

    Add files via upload
    This is a slightly different way to solve problem 13. Instead of using a random integer (in range length of list - 1) to grab a list item, the fortune is now determined with the random.choice(listName) function.

---
+++ PYTHONGaddisChapter7Problem13_02.py
@@ -0,0 +1,63 @@
+# Exercise 7.13 - Magic 8 Ball
+
+# Write a program that simulates a Magic 8 Ball, which is a 
+# fortune-telling toy that displays a random response to a 
+# yes or no question. In the student sample programs for this 
+# book, you will find a text file named 8_ball_responses. 
+# txtPreview the document. The file contains 12 responses, 
+# such as "I don't think so", "Yes, of course!", "I'm not sure", 
+# and so forth. The program should read the responses from the 
+# file into a list. It should prompt the user to ask a question, 
+# then display one of the responses, randomly selected from the list. 
+# The program should repeat until the user is ready to quit. 
+
+import random
+
+def main():
+    # Open a file for reading.
+    infile = open(r'#', 'r')
+
+    # Get its contents in a list.
+    myList = make_list(infile)
+
+    # Shake the Magic 8 Ball.
+    shake(myList)
+
+    # Close the file.
+    infile.close()
+
+# This function takes the file object and makes a list.
+def make_list(infile):
+
+    # Make an empty list.
+    myList = []
+
+    # Read the file object.
+    line = infile.readline()
+
+    # Loop through the file object, stipping away newline.
+    for line in infile:
+        line = line.strip()
+
+        # If there is content in line, append it to the list.
+        if line:
+            myList.append(line)
+    
+    # Return the list.
+    return myList
+
+# This function shakes the Magic 8 Ball.
+def shake(myList):
+    print('You found a Magic 8 Ball. Would you like to shake it?')
+    ask = 'Y'
+
+    while ask == 'Y':
+        ask = input('Enter Y if yes, anything else to exit.')
+        print('')
+        fortune = random.choice(myList)
+        print('Magic 8 Ball says...', fortune)
+        print('')
+        print('Would you like to shake it again?')
+
+# Call the main function.
+main()
